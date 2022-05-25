7ad03471cf36728ba0f55e4808c2823e5b0bc90f
micuat <micuat@gmail.com>
2022-05-25 11:19:55 +0200

    hydra to sound

--- public/example/main-thread-to-audioworklet/app.js
+++ public/example/main-thread-to-audioworklet/app.js
@@ -63,16 +63,18 @@ function render() {
   // if there is room in the ring buffer.
   while (window.audioWriter.available_write() > 128) {
     for (let i = 0; i < 128; i++) {
-      sine[i] = Math.sin(phase);
-      phase += (2 * Math.PI * frequency) / ctx.sampleRate;
-      if (phase > 2 * Math.PI) {
-        phase -= 2 * Math.PI;
-      }
+      sine[i] = Math.sin(i/64.0*2*Math.PI)
+      // sine[i] = Math.sin(i/64*2*Math.PI)
+      // sine[i] = Math.sin(phase)
+      // phase += (2 * Math.PI * 44100/64) / ctx.sampleRate;
+      // if (phase > 2 * Math.PI) {
+      //   phase -= 2 * Math.PI;
+      // }
     }
     window.audioWriter.enqueue(sine);
   }
 }
-requestAnimationFrame(render);
+// requestAnimationFrame(render);
 
 const start = $(".start")[0];
 start.onclick = function () {
@@ -84,3 +86,67 @@ start.onclick = function () {
     start.innerText = "Stop";
   }
 };
+
+
+
+function setup() {
+  createCanvas(64, 64);
+  pixelDensity(1)
+  background(0)
+  
+  for(let i = 0; i < width;i++) {
+    let v = color((sin(i*TAU/width)+1)/2*128+(sin(i*2*TAU/width)+1)/2*128);
+    for(let j = 0; j < height; j++) {
+      set(i, j, v);
+    }
+  }
+  updatePixels()
+  cnv=select("#hydra")
+}
+
+
+let row = 0, cnv;
+
+function draw() {
+  // let t = millis()*0.001
+  // for(let i = 0; i < width;i++) {
+  //   for(let j = 0; j < height; j++) {
+  //   let v = color((sin(i*TAU/width+j/width*4)+1)/2*255);
+  //     set(i, j, v);
+  //   }
+  // }
+  // updatePixels()
+
+  image(cnv, 0, 0, width, height)
+
+  loadPixels()
+  if (!window.audioWriter) {
+    return;
+  }
+  // Synthetize a simple sine wave so it's easy to hear glitches, continuously
+  // if there is room in the ring buffer.
+  while (window.audioWriter.available_write() > 128) {
+    let count = 0
+    for(let j = 0; j < 2; j+=1) {
+      for (let i = 0; i < width; i++) {
+        sine[count] = (pixels[(i + row*width) * 4 + 1]/128-1);
+        count++
+      }
+      row = (row + 1) % height;
+    }
+    window.audioWriter.enqueue(sine);
+  }
+
+
+}
+
+
+var canv = document.createElement("canvas")
+canv.id = "hydra"
+canv.width=600;
+canv.height=600;
+document.body.appendChild(canv)
+// create a new hydra-synth instance
+var hydra = new Hydra({ detectAudio: false,canvas: canv })
+osc(4, 0.1, 1.2).out()
+window.n=noise
